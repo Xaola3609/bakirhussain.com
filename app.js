457cef0c07c5eedf5b2e6dfa2cf14978fa38923f
Bakir Hussain <bhussain19977@gmail.com>
2023-05-07 13:51:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.Controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const sectBtn = document.querySelectorAll('.control-button');
+const allSections = Array.from(document.querySelectorAll('.main-content'));
 
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener('click', function () {
+      let currentBtn = document.querySelectorAll('.active-button');
+      currentBtn[0].className = currentBtn[0].className.replace('active-button', '');
+      this.className += ' active-button';
+    });
+  }
+  // sections active
+  allSections.forEach((section) => {
+    section.addEventListener('click', (e) => {
+      const id = e.target.parentNode.dataset.id;
+      if (id) {
+        sectBtns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+        e.target.parentNode.classList.add('active');
+        sections.forEach((section) => {
+          section.classList.remove('active');
+        });
 
-function PageTransitions(){
-	//Button click active class
-	for(let i = 0; i < sectBtn.length; i++){
-		sectBtn[i].addEventListener('click', function() {
-			let currentBtn = document.querySelectorAll('.active-btn');
-			currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-			this.className += ' active-btn';
-		})
-	}
-	// sections active
-	allSections.forEach((section) => {
-		section.addEventListener('click', (e) => {
-		const id = e.target.dataset.id; 
-		if(id){
-
-			sectBtns.forEach((btn) => {
-				btn.classList.remove('active')
-			}) 
-			e.target.classList.add('active')
-			sections.forEach((section) => {
-				section.classList.remove('active')
-			})
-
-			const element = document.getElementById(id);
-			element.classList.add('active'); 
-		}
-	})
-	})
+        const element = document.getElementById(id);
+        element.classList.add('active');
+      }
+    });
+  });
 }
 
 PageTransitions();
